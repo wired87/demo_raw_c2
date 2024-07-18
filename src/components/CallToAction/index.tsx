3ff66796084850925d5416431220b1cb9c0710f6
wired87 <derbenedikt.sterra@gmail.com>
2024-07-18 22:16:52 +0200

    update

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -9,21 +9,15 @@ const CallToAction = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[570px] text-center">
                 <h2 className="mb-2.5 text-3xl font-bold text-white md:text-[38px] md:leading-[1.44]">
-                  <span>What Are You Looking For?</span>
-                  <span className="text-3xl font-normal md:text-[40px]">
-                    {" "}
-                    Get Started Now{" "}
-                  </span>
+                  <span>Are you ready for it?</span>
+
                 </h2>
-                <p className="mx-auto mb-6 max-w-[515px] text-base leading-[1.5] text-white">
-                  There are many variations of passages of Lorem Ipsum but the
-                  majority have suffered in some form.
-                </p>
+
                 <Link
                   href="/"
                   className="inline-block rounded-md border border-transparent bg-secondary px-7 py-3 text-base font-medium text-white transition hover:bg-[#0BB489]"
                 >
-                  Start using Play
+                  Shop now!
                 </Link>
               </div>
             </div>
