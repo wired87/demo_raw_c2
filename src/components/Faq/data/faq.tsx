20091f22b2d7f015677b98f77c6b222f8295842a
wired87 <derbenedikt.sterra@gmail.com>
2024-07-20 17:58:23 +0200

    update

--- src/components/Faq/data/faq.tsx
+++ src/components/Faq/data/faq.tsx
@@ -1,20 +1,21 @@
 import {DataTypes} from "@/components/Faq/data/dataInterface";
+import {GiGearStickPattern} from "react-icons/gi";
 
 export const faqContent: DataTypes[] = [
   {
     text: "How to use TailGrids?",
     paragraph: "It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available .",
-    icon: () => <YourIconComponent />, // Replace YourIconComponent with your actual icon component if needed
+    icon: () => <GiGearStickPattern />, // Replace YourIconComponent with your actual icon component if needed
   },
   {
     text: "How to download icons from LineIcons?",
     paragraph: "It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available .",
-    icon: () => <YourIconComponent />, // Replace YourIconComponent with your actual icon component if needed
+    icon: () => <GiGearStickPattern />, // Replace YourIconComponent with your actual icon component if needed
   },
   {
     text: "Is GrayGrids part of UIdeck?",
     paragraph: "It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available .",
-    icon: () => <YourIconComponent />, // Replace YourIconComponent with your actual icon component if needed
+    icon: () => <GiGearStickPattern />, // Replace YourIconComponent with your actual icon component if needed
   },
 ];
 
@@ -22,16 +23,16 @@ export const faqContent2: DataTypes[] = [
   {
     text: "How to use TailGrids?",
     paragraph: "It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available .",
-    icon: () => <YourIconComponent />, // Replace YourIconComponent with your actual icon component if needed
+    icon: () => <GiGearStickPattern />, // Replace YourIconComponent with your actual icon component if needed
   },
   {
     text: "How to download icons from LineIcons?",
     paragraph: "It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available .",
-    icon: () => <YourIconComponent />, // Replace YourIconComponent with your actual icon component if needed
+    icon: () => <GiGearStickPattern />, // Replace YourIconComponent with your actual icon component if needed
   },
   {
     text: "Is GrayGrids part of UIdeck?",
     paragraph: "It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available .",
-    icon: () => <YourIconComponent />, // Replace YourIconComponent with your actual icon component if needed
+    icon: () => <GiGearStickPattern />, // Replace YourIconComponent with your actual icon component if needed
   },
 ]
