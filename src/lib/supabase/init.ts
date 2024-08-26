244c6e300fa8ee245e7760fbff1081aa05127404
wired87 <derbenedikt.sterra@gmail.com>
2024-08-26 19:22:03 +0200

    su page

--- src/lib/supabase/init.ts
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://bvvpjxnzhahifdphhqpr.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE!
-export const supabase = createClient(supabaseUrl, supabaseKey)
