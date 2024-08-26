7d1103cd86cfa9641935ed8bd624c9e9660b583d
wired87 <derbenedikt.sterra@gmail.com>
2024-08-26 21:00:05 +0200

    su page

--- src/components/c1/Specifications.tsx
+++ src/components/c1/Specifications.tsx
@@ -1,4 +1,7 @@
 import {LeftTopDots} from "@/components/dot/leftTop";
+import SingleFaq from "@/components/Faq/SingleFaq";
+import { CiSettings } from "react-icons/ci";
+import {GiGearStickPattern} from "react-icons/gi";
 
 /*
 "10 EEG, 2 ECG Electrodes and a 9-Axis chip provide a detailed insight about your mental and " +
@@ -7,21 +10,25 @@ import {LeftTopDots} from "@/components/dot/leftTop";
 
 const data = [
   {
+    icon: (<CiSettings size={40} color={"white"} />),
     title: "Round about monitoring",
     text: "10 EEG, 2 ECG Electrodes and a 9-Axis chip provide a detailed insight about your mental and " +
       "physical state and transform your the C-1-BCI to a next gen health tracker.",
   },
   {
+    icon:  (<CiSettings size={40} color={"white"} />),
     title: "Comfort Design",
     text: "The unobtrusive design brings the benefits from modern BCIs to your daily life",
 
   },{
+    icon:  (<CiSettings size={40} color={"white"} />),
     title: "Adaptive Learning",
     text: "The C-1-BCI continuously learns from you " +
       " and ensures that the device becomes more attuned to your " +
       "individual needs.",
   },
   {
+    icon:  (<CiSettings size={40} color={"white"} />),
     title: "Seamless Connectivity",
     text: "Effortlessly sync your C-1-BCI with your smartphone or tablet " +
       "to interact with your favorite health apps and smart home devices or " +
@@ -35,11 +42,12 @@ export const Specifications = () => {
     <div className={"md:w-1/2 w-full flex-col relative flex justify-start items-start px-9"}>
       <ul className={"w-full flex h-full gap-y-6 justify-start items-baseline flex-col"}>
         {data.map((item, i) => (
-          <li className={"flex gap-y-2 mb-8 flex-col items-start justify-start"} key={i}>
-            <p
-              className={" text-3xl font-semibold text-dark dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"}>{item.title}</p>
-            <p className={"text-left text-lg font-semibold"}>{item.text}</p>
-          </li>
+          <SingleFaq
+            key={i}
+            question={item.title}
+            answer={item.text}
+            icon={item.icon}
+          />
         ))}
       </ul>
       <LeftTopDots/>
